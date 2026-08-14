91b517006d143fa167aa1074a393bb3245ab8bdc
jmin4078 <wpalsrl8588@gmail.com>
2026-08-14 10:15:10 +0900

    Implement board creation service

---
+++ src/main/java/org/example/rest/controller/BoardController.java
@@ -0,0 +1,9 @@
+package org.example.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+}
